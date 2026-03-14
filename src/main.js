150e142ac02f7587ed8b548b75fa2665c63d1b53
occiandiaali <occiandiaali@gmail.com>
2026-03-14 17:21:28 +0100

    Add routes for about and terms;adjust styling

--- src/main.js
+++ src/main.js
@@ -10,6 +10,10 @@ function router() {
     renderGame(cat);
   } else if (route === "/dashboard") {
     renderDashboard();
+  } else if (route === "/about") {
+    renderAbout();
+  } else if (route === "/policy") {
+    renderPolicy();
   } else {
     renderCategorySelect();
   }
@@ -105,6 +109,7 @@ function renderGame(category) {
   data.hints.forEach((hint) => {
     const btn = document.createElement("button");
     btn.textContent = hint.q;
+
     btn.onclick = () => {
       if (hint.a) {
         if (filterLevel < filters.length - 1) {
@@ -117,6 +122,7 @@ function renderGame(category) {
         status.textContent = `Wrong hint. Guesses left: ${guessesLeft}`;
       }
       if (guessesLeft <= 0) {
+        btn.style.display = "none"; // Try to prevent firing hints
         endGame(false, data.name, category);
       }
     };
@@ -231,10 +237,10 @@ function renderDashboard() {
       <img
     src="/face-guess.svg"
     alt="Face Guess"
-    width="200px"
-    height="200px"
+    width="60px"
+    height="60px"
     />
-  <h1>Dashboard</h1>
+  <h2>Dashboard</h2>
       <p>Global streak: ${streak} day(s)</p>
       <p>Science streak: ${scienceStreak} day(s)</p>
       <p>Movies streak: ${moviesStreak} day(s)</p>
@@ -252,8 +258,61 @@ function renderDashboard() {
 `;
 }
 
-/**
- *     <p>Current streak: ${streak} day(s)</p>
-    <p>Badges coming soon!</p>
-    <button onclick="window.location.hash='/'">Back to categories</button>
- */
+function renderAbout() {
+  app.innerHTML = `
+  <button onclick="window.location.hash='/'">Back</button>
+  <h2>About Us</h2>
+  <p>
+  We are everyday people who're enthusiastic about the identities and life-stories of humans who have excelled, or simply fascinate us. This site was launched in 2026.
+  This site is owned and operated by occiandiaali.com, in association with id8 Media.
+  </p>
+  <h2>Contact Us</h2>
+  <form action="" method="get" class="form-example">
+  <div class="form-example">
+    
+    <input type="email" name="email" id="email" placeholder="Your Email" required />
+  </div>
+    <div class="form-example">
+    <textarea rows="4" cols="15" name="message" id="message" placeholder="Message here.."></textarea>
+    
+  </div>
+
+</form>
+  `;
+}
+
+function renderPolicy() {
+  app.innerHTML = `
+  <button onclick="window.location.hash='/'">Back</button>
+  <h2>Privacy Policy</h2>
+  <p>
+  This page is used to inform website visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service.
+
+If you choose to use our Service, then you agree to the collection and use of information in relation with this policy. The Personal Information that we collect are used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.
+</p>
+<p>
+<strong>Information Collection and Use</strong>
+</p>
+<p>
+We take and respect your privacy seriously. We may collect information via cookie or web log. This is to customize services and enhance customer satisfaction.
+
+The data we collect are:
+</p>
+<p>
+<strong>Log Data</strong>
+</p>
+<p>
+We want to inform you that whenever you visit our Service, we collect information that your browser sends to us that is called Log Data. This Log Data may include information such as your computer's Internet Protocol ("IP") address, browser version, pages of our Service that you visit, the time and date of your visit, the time spent on those pages, and other statistics.
+</p>
+<p>
+<strong>Cookies and Third Party Advertising</strong>
+</p>
+<p>
+Cookies are files with small amount of data that is commonly used an anonymous unique identifier. These are sent to your browser from the website that you visit and are stored on your computer's hard drive.
+
+Our website uses these "cookies" to collection information and to improve our Service. You have the option to either accept or refuse these cookies, and know when a cookie is being sent to your computer. If you choose to refuse our cookies, you may not be able to use some portions of our Service.
+
+We allow third-party companies to serve ads and/or collect certain anonymous information when you visit our web site. These companies may use non-personally identifiable information (e.g., click stream information, browser type, time and date, subject of advertisements clicked or scrolled over) during your visits to this and other Web sites in order to provide advertisements about goods and services likely to be of greater interest to you. These companies typically use a cookie or third party web beacon to collect this information. To learn more about this behavioral advertising practice or to opt-out of this type of advertising, you can visit optout.networkadvertising.org
+  </p>
+  `;
+}
